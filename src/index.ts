adda3359ada34607a0437b8c021378b9441e25bf
Robert <robertogarciadevelopment@gmail.com>
2020-11-19 23:28:24 +0100

    Improved port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "module-alias/register";
 import app from "./app";
 
-const FASTIFY_PORT = Number(process.env.PORT) || 3006;
+const FASTIFY_PORT = process.env.PORT || 3006;
 
-app.listen(FASTIFY_PORT, err => {
+app.listen(FASTIFY_PORT, "0.0.0.0", (err, address) => {
   if (err) console.log("Cannot start Fastify server", err);
-  console.log(`\nFastify server running on port ${FASTIFY_PORT}`);
+  console.log(`\nFastify server running on ${address}`);
 });
